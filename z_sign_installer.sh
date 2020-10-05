96615651aa6b37f47afd81d30846f85f1f54cf7a
Noah Baculi <nbaculi@hotmail.com>
2020-10-05 16:50:04 -0400

    Renamed code signer script[D[D[D[D[D[Dbash script and deleted unsuccessful shell script (see previous commit)

--- z_sign_installer.sh
+++
@@ -1,4 +0,0 @@
-signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a "installer_windwows/Output/aldras-setup-2020-2.exe"
-
-the below functions
-signtool sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll /v "C:/Users/Noah Baculi/Documents/aldras-app/installer_windows/Output/aldras-setup-2020-2.exe"
